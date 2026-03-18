c21f056a179d8b5a03ea4aef942df4bac51b106b
Gabriel Simon <gabrielsimonbr@hotmail.com>
2026-03-17 23:11:34 -0300

    refactor: remove unused imports and clean up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home'
 import NotFound from './pages/not-found'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
